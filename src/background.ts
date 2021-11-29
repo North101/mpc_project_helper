563b02854804bfa66ff55b3adddf1e678f8554ea
Alexander Wilde <alexander.wilde87@gmail.com>
2021-11-29 17:16:26 +0000

    Change mimetype to txt

--- src/background.ts
+++ src/background.ts
@@ -25,7 +25,7 @@ chrome.action.onClicked.addListener((tab: any) => {
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === 'download') {
     chrome.downloads.download({
-      url: `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(request.value))}`,
+      url: `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(request.value))}`,
       filename: 'project.txt',
     });
   } else if (request.message === 'open') {
