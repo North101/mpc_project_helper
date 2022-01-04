d177cb1509e5f92eba6569c7ecc541c518340d69
Alexander Wilde <alexander.wilde87@gmail.com>
2022-01-04 13:44:51 +0000

    Add warning

--- src/components/ProjectSuccessModal.tsx
+++ src/components/ProjectSuccessModal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Alert from "react-bootstrap/esm/Alert";
 import Button from "react-bootstrap/esm/Button";
 import Modal from "react-bootstrap/esm/Modal";
 
@@ -9,7 +10,11 @@ export default class ProjectSuccessModal extends React.Component<{ value: string
       <Modal show={true} centered={true}>
         <Modal.Header>Success</Modal.Header>
         <Modal.Body>
-          Your project was successfully uploaded
+          Your project was successfully uploaded.
+          <Alert variant="warning">
+            Warning: MPC Helper is reverse engineered and makes not guaranties that it is bug free or that {location.origin} will make changes that break things.
+            Always check the uploaded project and its images before purchasing.
+          </Alert>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.props.onClose}>Close</Button>
