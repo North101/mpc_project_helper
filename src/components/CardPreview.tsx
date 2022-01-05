57eb9f30a6d414e385e4df09ec5c310cfa37a850
Alexander Wilde <alexander.wilde87@gmail.com>
2022-01-05 23:48:12 +0000

    Stylying fixes for card preview

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -25,17 +25,17 @@ export default class CardPreview extends React.Component<CardPreviewProps, CardP
   render() {
     const { height, width, card } = this.props;
     return (
-      <div style={{ padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 4, border: '1px dashed' }}>
-        {card.name}
+      <div style={{ width: width + 16, padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 4, border: '1px dashed' }}>
+        <span style={{ textOverflow: 'clip', overflow: 'hidden', whiteSpace: 'nowrap', width: '100%' }}>{card.name}</span>
         <div style={{ display: 'flex', gap: 4, }}>
-          <div style={{ display: 'flex', justifyItems: 'center', height: height / 2, width: width / 2 }}>
+          <div style={{ display: 'flex', justifyContent: 'center', height: height / 2, width: width / 2 }}>
             <img src={card.front ? URL.createObjectURL(card.front.file) : ''}></img>
           </div>
-          <div style={{ display: 'flex', justifyItems: 'center', height: height / 2, width: width / 2 }}>
+          <div style={{ display: 'flex', justifyContent: 'center', height: height / 2, width: width / 2 }}>
             <img src={card.back ? URL.createObjectURL(card.back.file) : ''}></img>
           </div>
         </div>
-      </div>
+      </div >
     );
   }
 }
