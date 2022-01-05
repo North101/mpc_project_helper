fa3cd87ea2fffbf2cc0d9079f8be436971c471d4
Alexander Wilde <alexander.wilde87@gmail.com>
2022-01-05 13:50:20 +0000

    Fix warning

--- src/components/ProjectSuccessModal.tsx
+++ src/components/ProjectSuccessModal.tsx
@@ -13,7 +13,7 @@ export default class ProjectSuccessModal extends React.Component<{ value: string
           <p>Your project was successfully uploaded.</p>
           <Alert variant="warning">
             <Alert.Heading>Warning</Alert.Heading>
-            <p>MPC Project Helper is reverse engineered and makes no guaranties that it is bug free or that {location.origin} will make changes that will break it.</p>
+            <p>MPC Project Helper is reverse engineered and makes no guaranties that it is bug free or that {location.origin} will not make changes that will break it.</p>
             <hr />
             <p>Always check the uploaded project and its images before purchasing.</p>
           </Alert>
