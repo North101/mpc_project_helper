92233a4fabd2560801c90a059f6ea978a055a5eb
Alexander Wilde <alexander.wilde87@gmail.com>
2022-01-11 02:09:53 +0000

    Consistent quoting

--- src/content.tsx
+++ src/content.tsx
@@ -5,12 +5,12 @@ import App from "./components/App";
 import "./popup.css";
 
 const head = document.head;
-const link = document.createElement("link");
-link.type = "text/css";
-link.rel = "stylesheet";
-link.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-link.integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T";
-link.crossOrigin = "anonymous";
+const link = document.createElement('link');
+link.type = 'text/css';
+link.rel = 'stylesheet';
+link.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+link.integrity = 'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T';
+link.crossOrigin = 'anonymous';
 head.appendChild(link);
 
 const app = document.createElement('div');
