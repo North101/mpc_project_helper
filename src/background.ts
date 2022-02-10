4b1b5052bfb7934fda54a74cbe3ba7ffc949b2e0
Alexander Wilde <alexander.wilde87@gmail.com>
2022-02-10 15:38:51 +0000

    Remove unused permissions

--- src/background.ts
+++ src/background.ts
@@ -23,12 +23,7 @@ chrome.action.onClicked.addListener((tab: any) => {
 });
 
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.message === 'download') {
-    chrome.downloads.download({
-      url: `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(request.value))}`,
-      filename: 'project.txt',
-    });
-  } else if (request.message === 'open') {
+  if (request.message === 'open') {
     chrome.tabs.create({
       url: request.url,
     });
