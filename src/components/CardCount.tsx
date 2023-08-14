67c1558bd31133d899b6bbee2ac0b7c878048f5f
Alexander Wilde <alexander.wilde87@gmail.com>
2023-08-14 01:33:40 +0100

    Updated card count tooltip

--- src/components/CardCount.tsx
+++ src/components/CardCount.tsx
@@ -9,7 +9,10 @@ interface CardCountProps {
 const CardCount = ({ count, unit }: CardCountProps) => (
   <div className='text-end'>
     <FilenameTooltip text={unit ? `Card Count: ${count} / ${unit.maxCards}` : `Card Count: ${count}`}>
-      The max amount of cards a project can have is {unit?.maxCards ?? 'Unknown'}.<br />
+      {unit
+        ? <>The max number of cards a project can have is {unit.maxCards}.</>
+        : <>Choose a Product to see the max card count a project can have.</>
+      }<br />
       If you add more than {unit?.maxCards ?? 'the max'} then the project will be automatically split into multiple projects.
     </FilenameTooltip>
   </div>
