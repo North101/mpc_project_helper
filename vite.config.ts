4694bb8150262dab2144ec6cf15b74fa217ac2ce
Alexander Wilde <alexander.wilde87@gmail.com>
2023-11-26 23:19:52 +0000

    Upgrade libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react-swc'
-import path from 'node:path'
 import { defineConfig } from 'vite'
 import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
 
 function generateManifest() {
-  const manifest = readJsonFile('src/manifest.json')
+  const manifest = readJsonFile('manifest.json')
   const pkg = readJsonFile('package.json')
   return {
     name: pkg.name,
@@ -23,12 +22,4 @@ export default defineConfig({
       disableAutoLaunch: true,
     }),
   ],
-  resolve: {
-    alias: {
-      // In dev mode, make sure fast refresh works
-      '/@react-refresh': path.resolve(
-        'node_modules/@vitejs/plugin-react-swc/refresh-runtime.js'
-      ),
-    },
-  },
 })
