51f57862f38cf6bec8c97863056dbe6949d771b1
Alexander Wilde <alexander.wilde87@gmail.com>
2024-02-27 18:58:58 +0000

    Remove outdir on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import fs from 'node:fs/promises'
+import { PluginOption, ResolvedConfig, defineConfig } from 'vite'
 import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
+
+const rmOutDir = (): PluginOption => {
+  let viteConfig: ResolvedConfig
+
+  return {
+    name: 'vite-plugin-rm-outDir',
+    configResolved: (resolvedConfig) => {
+      viteConfig = resolvedConfig
+    },
+    async buildStart() {
+      await fs.rm(viteConfig.build.outDir, {
+        recursive: true,
+        force: true,
+      })
+      await fs.mkdir(viteConfig.build.outDir, {
+        recursive: true,
+      })
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    rmOutDir(),
     react(),
     webExtension({
       transformManifest: (manifest) => {
