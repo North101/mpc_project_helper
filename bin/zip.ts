7e5a1e82bec9867ee614eae953d6f572a9986125
Alexander Wilde <alexander.wilde87@gmail.com>
2024-02-27 16:39:16 +0000

    Add command to zip extension

---
+++ bin/zip.ts
@@ -0,0 +1,3 @@
+import { zip } from "zip-a-folder";
+
+await zip("dist", "extension.zip");
--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,20 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
 
-
-function generateManifest() {
-  const manifest = readJsonFile('manifest.json')
-  const pkg = readJsonFile('package.json')
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  }
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     webExtension({
-      manifest: generateManifest,
-      disableAutoLaunch: true,
+      transformManifest: (manifest) => {
+        const pkg = readJsonFile('package.json')
+        return {
+          name: pkg.name,
+          description: pkg.description,
+          version: pkg.version,
+          ...manifest,
+        }
+      },
     }),
   ],
 })
